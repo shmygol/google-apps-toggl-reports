0d6b9b7bdc9a26d5084241bbb179ff3cc149931e
Ilya Shmygol <ishmygol@gmail.com>
2016-03-30 14:08:15 +0200

    Issue #1: Wrap sheets and utils libraries to functions

--- src/libs/lib.utils.js
+++ src/libs/lib.utils.js
@@ -1,25 +1,47 @@
 
-var utils = utils || {};
+function require_lib_utils_() {
+  var utils = {};
 
-/**
- * Logs an Apps Script exception, including the call stack
- * @private
- * @param {Object} e Apps Script runtime error object
- */
-utils.logException = function(e) {
-  Logger.log('Apps Script runtime exception:');
-  Logger.log(e.message);
-  Logger.log('\n' + e.stack + '\n');
-};
+  /**
+   * Composes the correct URL to open a Google Sheet from Drive by URL.
+   * @param {String} fileId Drive file ID for the spreadsheet.
+   * @param {String} gid The sheet ID to open.
+   *     Value is in "gid=" param in the URL.
+   * @return {String} Full URL to open the spreadsheet on Drive.
+   */
+  utils.getSpreadsheetUrl = function(fileId, gid) {
+    if (typeof fileId === 'undefined' || typeof gid === 'undefined') {
+      throw new TypeError('Both fileId and gid parameters must be defined');
+    }
+    return 'https://docs.google.com/spreadsheets/d/' + fileId +
+        '/edit#gid=' + gid;
+  }
 
-/**
- *  Used to insert any HTML file in the project into an outer HTML file.
- *  Called from within the outer HTML file.
- *  @param {String} filename Name of the file in the project.
- *      Do not include ".html".
- *  @return {String} HTML markup for the requested file.
- */
-utils.includeUiFile = function(filename) {
-  return HtmlService.createHtmlOutputFromFile(filename).getContent();
-};
+  /**
+   * Logs an Apps Script exception, including the call stack
+   * @private
+   * @param {Object} e Apps Script runtime error object
+   */
+  utils.logException = function(e) {
+    Logger.log('Apps Script runtime exception:');
+    Logger.log(e.message);
+    Logger.log('\n' + e.stack + '\n');
+  };
 
+  /**
+   *  Used to insert any HTML file in the project into an outer HTML file.
+   *  Called from within the outer HTML file.
+   *  @param {String} filename Name of the file in the project.
+   *      Do not include ".html".
+   *  @return {String} HTML markup for the requested file.
+   */
+  utils.includeUiFile = function(filename) {
+    return HtmlService.createHtmlOutputFromFile(filename).getContent();
+  };
+
+  return utils;
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = require_lib_utils_();
+}
